0be44f0b1a3c9845495b549187adf61c3a2290b7
Sébastien Demanou <demsking@gmail.com>
2017-04-23 19:43:45 +0200

    Add CLI option --ignore-name and --ignore-description

--- bin/cli.js
+++ bin/cli.js
@@ -2,14 +2,55 @@
 
 'use strict'
 
+const MISSING_FILENAME_MESSAGE = 'Missing filename. Usage: vuedoc.md [*.vue files]...'
+
 if (process.argv.length < 2) {
-  return console.error('Missing filename. Usage: vuedoc.md [*.vue files]...')
+  return console.error(MISSING_FILENAME_MESSAGE)
 }
 
 const vuedoc = require('../')
 const options = { stdout: true }
 
-process.argv.slice(2).forEach((filename) => {
+const argv = process.argv.slice(2)
+const filenames = []
+
+for (let i = 0; i < argv.length; i++) {
+  const arg = argv[i]
+
+  switch (arg) {
+    case '--level':
+      if (!argv[i + 1]) {
+        return console.error('Missing level value. Usage: --level [integer]')
+      }
+
+      const value = parseInt(argv[i + 1])
+
+      if (isNaN(value)) {
+        return console.error('Invalid level value. Usage: --level [integer]')
+      }
+
+      options.level = value
+      i++
+      continue
+
+    case '--ignore-name':
+      options.ignoreName = true
+      continue
+
+    case '--ignore-description':
+      options.ignoreDescription = true
+      continue
+
+    default:
+      filenames.push(arg)
+  }
+}
+
+if (filenames.length === 0) {
+  return console.error(MISSING_FILENAME_MESSAGE)
+}
+
+filenames.forEach((filename) => {
   options.filename = filename
 
   vuedoc.md(options).catch((e) => console.error(e))
