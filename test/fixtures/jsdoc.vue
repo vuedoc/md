72c30bb933772e76f85c08e1658e4d184640b7f5
Sébastien Demanou <demsking@gmail.com>
2018-06-03 17:12:58 +0200

    Add test for jsdoc rendering

---
+++ test/fixtures/jsdoc.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+    name: 'checkbox',
+    methods: {
+      /**
+      * Set the checkbox ID
+      * @param {string} id - The checkbox ID
+      * @param {string} [name] - The checkbox name
+      * @param {number} [order=1] - The checkbox order
+      * @param {string|string[]} [values] - The checkbox values
+      * @param {*} [...rest] - The rest options
+      * @return {boolean} True on success; ortherwise false
+      */
+      set (id, name, ...rest) {
+        const status = true
+        /**
+        * Emitted the event `finished` when loaded
+        * Multilign
+        * @param {*} status - The finishing status
+        */
+        this.$emit('finished', status)
+      }
+    }
+  }
+</script>
--- test/specs/lib/cli.spec.js
+++ test/specs/lib/cli.spec.js
@@ -526,4 +526,17 @@ describe('lib/cli', () => {
       cli.silenceExec([notfoundfile])
     })
   })
+
+  describe('integration', () => {
+    describe('jsdoc', () => {
+      it('should successfully render with JSDoc tags', () => {
+        const filename = path.join(fixturesPath, 'jsdoc.vue')
+        const expected = '# checkbox \n\n## events \n\n- `finished` \n\n   Emitted the event `finished` when loaded Multilign \n\n   **arguments:** \n\n     - `status` **Any** - The finishing status \n\n## methods \n\n- `set(id, name, order, values, ...rest)` \n\n   Set the checkbox ID \n\n   **parameters:** \n\n     - `id` **string** - The checkbox ID \n     - `name` **string** *(optional)* - The checkbox name \n     - `order` **number** *(optional)* `default: 1` - The checkbox order \n     - `values` **string|string[]** *(optional)* - The checkbox values \n     - `...rest` **Any** *(optional)* - The rest options \n\n   **return value:** \n\n     - **boolean** - True on success; ortherwise false \n'
+        const options = { filename }
+
+        return cli.exec([ filename ])
+          .then(() => assert.equal(streamContent, expected))
+      })
+    })
+  })
 })
